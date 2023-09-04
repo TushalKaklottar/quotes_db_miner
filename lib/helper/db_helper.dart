e678555b3373c494c0b9cddf142923616b75e525
tushal kaklotar <tushalkaklotar90@gmail.com>
2023-09-04 10:37:17 +0530

    KT || 04/09/2023

--- lib/helper/db_helper.dart
+++ lib/helper/db_helper.dart
@@ -25,7 +25,7 @@ class DB_helper {
 
   init() async {
     String dbPath = await getDatabasesPath();
-    String dbName = 'qu.db';
+    String dbName = 'qut.db';
     String path = join(dbPath,dbName);
 
 
